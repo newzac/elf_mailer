3d70998907514aca4e50a6f609c6906234e95231
Zachary Newman <gpg@znewman.com>
2019-11-28 11:41:41 -0600

    add docs to postal service

--- lib/postal_service.rb
+++ lib/postal_service.rb
@@ -2,6 +2,18 @@ require 'rest-client'
 
 module ElfMailer
   class PostalService
+
+    # #
+    # Posts the email message and payload to the configured mailer url
+    # Params:
+    # +email_info+ is hash of email configuration info including
+    #   :mailer_url - string - Mailer provider url to post the message info to
+    #   :from_name - string - the name to provide as the from name
+    #   :from_address - string - the email address to set as the from email
+    #   :to_address - string - the email address to send the email to
+    #   :subject - string - the subject of the email
+    #   :message - string - the message body for the email
+
     def self.send_message(email_info)
       RestClient::Request.execute(
         :url => ENV['mailer_url'],
@@ -16,6 +28,12 @@ module ElfMailer
       )
     end
 
+    # #
+    # Generates and returns the message body to be used in the mailer elf email
+    # Params:
+    # +secret_santa+ - string - the name of the person recieving the email
+    # +person+ - string - the person that the secret santa has been matched to
+    # +additonal_message+ - string - any additional text that you would like to add in the message
 
     def self.form_message(secret_santa, person)
       """Hello #{secret_santa},
